b3e0dc9b483803ed9111efc3c1f110a0b606ac7d
ULTRAPROGRAMMERGEORGE <mrgoldy.ilin@yandex.ru>
2025-01-29 02:32:15 +0300

    V.-20.1
    So I tried to gave responses in order of choices in a pop up window. I like the speed, allas i'm interted in future changing the view of window

--- start.cpp
+++ start.cpp
@@ -1,9 +1,33 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <list>
+#include <iostream>
+#include <string>
+#include <list>
+#include <cmath>
+#include <conio.h>
 #include <Windows.h>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <sstream>
+#include <iterator>
+#include <iomanip>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
-    int i = MessageBox(NULL, "TEXT", "TEXT", MB_OKCANCEL);
-    cin.get();
-    return i;
+    int i = MessageBox(0, "TEXT", "TEXT", MB_OKCANCEL);
+    if (i == IDOK){
+        printf("%d\n L");
+    }
+    else if (i == IDCANCEL){
+        printf("%d\n K");
+    }
 }
