db01273d68552b8d6ba482e1eb7727a7c65f99e3
ULTRAPROGRAMMERGEORGE <mrgoldy.ilin@yandex.ru>
2025-02-08 19:43:42 +0300

    v.-1.5.0

--- df.cpp
+++ df.cpp
@@ -1,8 +1,8 @@
 #include "ssh.h"
 int main(){
-    const char *io = "193.34.213.252";
+    const char *io = "-";
     const char *uo = "root";
-    const char *po = "&DrS5w$rz100";
+    const char *po = "-";
     Connect c1{io,uo,po};
     c1.file();
     return 0;
